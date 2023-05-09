b810bda6b77398024b3fa40f73205d08d038ffcb
Lucas Jacquemin <Lucas.jacquemin2002@gmail.com>
2023-05-09 19:29:09 +0200

    basic tilemap + net action

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -18,6 +18,7 @@ public class PlayerController : MonoBehaviour
     private float horizontal, vertical;
     private Rigidbody2D playerRigidbody;
     public Animator animator;
+    public Animator animatorTool;
 
     public float playerMoveSpeed;
 
@@ -31,11 +32,20 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         playerRigidbody = gameObject.GetComponent<Rigidbody2D>();
+        //Animator animator2 = GetComponentInChildren<Animator>();
+
+        foreach (Transform child in transform)
+        {
+            if (child.name == "CurrentTool") {
+                animatorTool = (Animator)child.GetComponent(typeof(Animator));
+            }
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+
         //move character in the right direction by applying force
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
@@ -47,8 +57,18 @@ public class PlayerController : MonoBehaviour
         //playerRigidbody.AddRelativeForce(vector*playerMoveSpeed);
 
 
-        animator.SetFloat("Horizontal", horizontal);
-        animator.SetFloat("Vertical", vertical);
+        if (vector != Vector2.zero) {
+            animator.SetFloat("Horizontal", horizontal);
+            animator.SetFloat("Vertical", vertical);
+        }
         animator.SetFloat("Speed", new Vector2(horizontal,vertical).magnitude);
+
+        if (vector != Vector2.zero)
+        {
+            animatorTool.SetFloat("Horizontal", horizontal);
+            animatorTool.SetFloat("Vertical", vertical);
+        }
+        if(Input.GetMouseButtonDown(0) == true) animatorTool.SetTrigger("Use");
+
     }
 }
